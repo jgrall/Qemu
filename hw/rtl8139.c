fd15f5551155e805db16788de92d24c871268750
Julien Grall <julien.grall@citrix.com>
2012-06-20 00:20:21 +0100

    Revert "rtl8139: disable pxe rom when xen is enabled"
    This reverts commit 041b588a5a7a3f6a2f01f0e1886dbbb8a7824fa8.

--- hw/rtl8139.c
+++ hw/rtl8139.c
@@ -59,7 +59,6 @@
 #include "loader.h"
 #include "sysemu/sysemu.h"
 #include "qemu/iov.h"
-#include "xen.h"
 
 /* debug RTL8139 card */
 //#define DEBUG_RTL8139 1
@@ -3531,9 +3530,7 @@ static void rtl8139_class_init(ObjectClass *klass, void *data)
 
     k->init = pci_rtl8139_init;
     k->exit = pci_rtl8139_uninit;
-    if (!xen_enabled ()) {
-        k->romfile = "pxe-rtl8139.rom";
-    }
+    k->romfile = "pxe-rtl8139.rom";
     k->vendor_id = PCI_VENDOR_ID_REALTEK;
     k->device_id = PCI_DEVICE_ID_REALTEK_8139;
     k->revision = RTL8139_PCI_REVID; /* >=0x20 is for 8139C+ */
